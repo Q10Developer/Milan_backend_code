ed3174cbeb9b789359c3a261a7a1c2d0d5a69cec
Girish Pant <Girish Pant>
2023-06-18 13:37:28 +0530

    Fix issue of ObservationEntity

--- src/main/java/com/app/user/entity/ObservationEntity.java
+++ src/main/java/com/app/user/entity/ObservationEntity.java
@@ -29,9 +29,6 @@ public class ObservationEntity {
 
 	@Column(name = "active_status")
 	private int activeStatus;
-	
-	@Column(name="registration_no")
-	private Long registrationNo;
 
 	public Long getObservationId() {
 		return observationId;
@@ -80,15 +77,6 @@ public class ObservationEntity {
 	public void setActiveStatus(int activeStatus) {
 		this.activeStatus = activeStatus;
 	}
-	
-
-	public Long getRegistrationNo() {
-		return registrationNo;
-	}
-
-	public void setRegistrationNo(Long registrationNo) {
-		this.registrationNo = registrationNo;
-	}
 
 	@Override
 	public String toString() {
@@ -105,12 +93,8 @@ public class ObservationEntity {
 		builder.append(observationCategoryType);
 		builder.append(", activeStatus=");
 		builder.append(activeStatus);
-		builder.append(", registrationNo=");
-		builder.append(registrationNo);
 		builder.append("]");
 		return builder.toString();
 	}
-
-	
 
 }
