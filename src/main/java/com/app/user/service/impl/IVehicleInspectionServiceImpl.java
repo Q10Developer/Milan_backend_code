ffddac4d80a83a234e1872786d3cfc0e4c8deca4
Girish Pant <Girish Pant>
2023-09-14 08:12:09 +0530

    Calculations moved to Util class

--- src/main/java/com/app/user/service/impl/IVehicleInspectionServiceImpl.java
+++ src/main/java/com/app/user/service/impl/IVehicleInspectionServiceImpl.java
@@ -17,7 +17,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
-import com.app.user.constants.CalculationConstants;
 import com.app.user.constants.ResponseKeysValue;
 import com.app.user.constants.URLConstants;
 import com.app.user.dto.ServiceResponseDTO;
@@ -28,6 +27,7 @@ import com.app.user.entity.VehicleInspectionDetailsEntity;
 import com.app.user.entity.VehicleInspectionEntity;
 import com.app.user.repository.VehicleInspectionDetailsRepository;
 import com.app.user.repository.VehicleInspectionRepository;
+import com.app.user.utilities.CalculationUtils;
 import com.app.user.utilities.Utils;
 
 @Service
@@ -228,12 +228,13 @@ public class IVehicleInspectionServiceImpl {
 			/*
 			 * Other Calculations
 			 */
-			pressureAnalysis = calculatePressureAnalysis(requestDTO.getObsPressure(), requestDTO.getRecoPressure());
-			pressureAnalysisColorCode = calculatePressureAnalysisColorCode(pressureAnalysis);
-			leastTireThicknessAllowedAnalysis = calculateThicknessAnalysis(requestDTO.getRstMm(),
+			pressureAnalysis = CalculationUtils.calculatePressureAnalysis(requestDTO.getObsPressure(),
+					requestDTO.getRecoPressure());
+			pressureAnalysisColorCode = CalculationUtils.calculatePressureAnalysisColorCode(pressureAnalysis);
+			leastTireThicknessAllowedAnalysis = CalculationUtils.calculateThicknessAnalysis(requestDTO.getRstMm(),
 					requestDTO.getLeastTireThicknessAllowed());
-			wearAnalysis = calculateWearAnalysis(requestDTO.getObservationCategoryLabel(), requestDTO.getLstMm(),
-					requestDTO.getCtMm(), requestDTO.getRstMm());
+			wearAnalysis = CalculationUtils.calculateWearAnalysis(requestDTO.getObservationCategoryLabel(),
+					requestDTO.getLstMm(), requestDTO.getCtMm(), requestDTO.getRstMm());
 			rtd = Arrays.stream(new double[] { requestDTO.getRstMm(), requestDTO.getCtMm(), requestDTO.getLstMm() })
 					.min().getAsDouble();
 			/*
@@ -243,8 +244,8 @@ public class IVehicleInspectionServiceImpl {
 					&& !Utils.compareDates(vehicleInspectionEntity.getInspectionDateTime(),
 							requestDTO.getTireOriginalFitmentDate())) {
 				try {
-					mileagePerMm = calculateMileagePerMM(vehicleInspectionEntity, requestDTO, rtd);
-					projectedMileage = calculateProjectedMileagePerMM(requestDTO, mileagePerMm);
+					mileagePerMm = CalculationUtils.calculateMileagePerMM(vehicleInspectionEntity, requestDTO, rtd);
+					projectedMileage = CalculationUtils.calculateProjectedMileagePerMM(requestDTO, mileagePerMm);
 				} catch (ArithmeticException aex) {
 					LOGGER.error(
 							"ArithmeticException occurred while doing computation of mileagePerMm and projectedMileage : {}",
@@ -259,82 +260,6 @@ public class IVehicleInspectionServiceImpl {
 		requestDTO.setRtd(rtd);
 		requestDTO.setMileagePerMm(mileagePerMm);
 		requestDTO.setProjectedMileage(projectedMileage);
-	}
-
-	private static Double calculateMileagePerMM(VehicleInspectionEntity vehicleInspectionEntity,
-			VehicleInspectionDetailsRequestDTO requestDTO, double rtd) {
-		return (vehicleInspectionEntity.getVehicleOdometerReading()
-				- (requestDTO.getOdometerReadingWhenFitted() != null ? requestDTO.getOdometerReadingWhenFitted() : 0.0))
-				/ ((requestDTO.getOtdMm() != null ? requestDTO.getOtdMm() : 0.0) - rtd);
-	}
-
-	private static Double calculateProjectedMileagePerMM(VehicleInspectionDetailsRequestDTO requestDTO,
-			double mileagePerMm) {
-		return ((requestDTO.getOtdMm() != null ? requestDTO.getOtdMm() : 0.0)
-				- requestDTO.getLeastTireThicknessAllowed()) * mileagePerMm;
-	}
-
-	private static String calculatePressureAnalysis(Double obsPressure, Double recoPressure) {
-		double pressurePercentage = (obsPressure / recoPressure) * 100;
-		if (pressurePercentage > 95 && pressurePercentage < 105) {
-			return CalculationConstants.PRESSURE_OK;
-		} else if (pressurePercentage > 90 && pressurePercentage < 95) {
-			return CalculationConstants.UNDER_INFLATION;
-		} else if (pressurePercentage < 90) {
-			return CalculationConstants.EXTREME_UNDERINFLATION;
-		} else if (pressurePercentage > 105 && pressurePercentage < 110) {
-			return CalculationConstants.OVER_INFLATION;
-		} else if (pressurePercentage > 110) {
-			return CalculationConstants.EXTREME_OVERINFLATION;
-		}
-		return null;
-	}
-
-	private static String calculatePressureAnalysisColorCode(String pressureAnalysis) {
-		if (!StringUtils.isEmpty(pressureAnalysis)) {
-			if (pressureAnalysis.equals(CalculationConstants.PRESSURE_OK)) {
-				return CalculationConstants.COLOR_GREEN;
-			} else if (pressureAnalysis.equals(CalculationConstants.UNDER_INFLATION)) {
-				return CalculationConstants.COLOR_YELLOW;
-			} else if (pressureAnalysis.equals(CalculationConstants.EXTREME_UNDERINFLATION)) {
-				return CalculationConstants.COLOR_RED;
-			} else if (pressureAnalysis.equals(CalculationConstants.OVER_INFLATION)) {
-				return CalculationConstants.COLOR_YELLOW;
-			} else if (pressureAnalysis.equals(CalculationConstants.EXTREME_OVERINFLATION)) {
-				return CalculationConstants.COLOR_RED;
-			}
-		}
-		return null;
-	}
-
-	private static String calculateThicknessAnalysis(Double rstMm, int leastTireThicknessAllowed) {
-		if (rstMm != null && leastTireThicknessAllowed == 3) {
-			double redThreshold = leastTireThicknessAllowed * 1.25;
-			double yellowThreshold = leastTireThicknessAllowed * 1.5;
-			if (rstMm < redThreshold) {
-				return CalculationConstants.COLOR_RED;
-			} else if (rstMm < yellowThreshold) {
-				return CalculationConstants.COLOR_YELLOW;
-			} else {
-				return CalculationConstants.COLOR_GREEN;
-			}
-		}
-		return null;
-	}
-
-	private static String calculateWearAnalysis(String observationCategoryLabel, Double lstMm, Double ctMm,
-			Double rstMm) {
-		if (!CalculationConstants.NORMAL.equalsIgnoreCase(observationCategoryLabel) && lstMm != null && ctMm != null
-				&& rstMm != null) {
-			if (lstMm < ctMm && ctMm < rstMm) {
-				return CalculationConstants.LEFT_SLIDE_SLOPE;
-			} else if (lstMm > ctMm && ctMm > rstMm) {
-				return CalculationConstants.RIGHT_SLIDE_SLOPE;
-			} else if (lstMm > ctMm && ctMm < rstMm) {
-				return CalculationConstants.CENTER_WEAR;
-			}
-		}
-		return null;
 	}
 
 }
